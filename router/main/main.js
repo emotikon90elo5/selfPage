46d290113a67235d2d0e13f17b7dd9f127a52550
emotikonowy <emotka90sima@gmail.com>
2023-03-09 16:58:02 +0100

    delate moongo

--- router/main/main.js
+++ router/main/main.js
@@ -8,41 +8,41 @@ router.get('/:lang/', async (req, res) => {
     if (req.params.lang != 'pl' && req.params.lang != 'en') {
         return res.status(400).render('err')
     }
-    await mongoose.connect(mongodbIP).then(async () => {
-        const joins = await mongoose.connection.db.collection('joins')
+    // await mongoose.connect(mongodbIP).then(async () => {
+    //     const joins = await mongoose.connection.db.collection('joins')
 
-        await joins.findOneAndUpdate(
-            { link: `/${req.params.lang}/` },
-            {
-                $inc: {
-                    joins: 1,
-                },
-            },
-            { new: true },
-        )
-        await joins.findOneAndUpdate(
-            { link: '/:lang/' },
-            { $inc: { joins: 1 } },
-            { new: true },
-        )
-    })
+    //     await joins.findOneAndUpdate(
+    //         { link: `/${req.params.lang}/` },
+    //         {
+    //             $inc: {
+    //                 joins: 1,
+    //             },
+    //         },
+    //         { new: true },
+    //     )
+    //     await joins.findOneAndUpdate(
+    //         { link: '/:lang/' },
+    //         { $inc: { joins: 1 } },
+    //         { new: true },
+    //     )
+    // })
 
     let input = {
         lang: req.params.lang,
         logined: req.session.loggedin,
         username: req.session.username,
     }
-    await mongoose.connect(mongodbIP).then(async () => {
-        const joins = await mongoose.connection.db.collection('joins')
-        await joins.find({ link: '/:lang/' }).toArray((err, result) => {
-            Object.assign(input, { all: result[0].joins })
-        })
-        await joins
-            .find({ link: `/${req.params.lang}/` })
-            .toArray((err, result) => {
-                Object.assign(input, { langJoins: result[0].joins })
-            })
-    })
+    // await mongoose.connect(mongodbIP).then(async () => {
+    //     const joins = await mongoose.connection.db.collection('joins')
+    //     await joins.find({ link: '/:lang/' }).toArray((err, result) => {
+    //         Object.assign(input, { all: result[0].joins })
+    //     })
+    //     await joins
+    //         .find({ link: `/${req.params.lang}/` })
+    //         .toArray((err, result) => {
+    //             Object.assign(input, { langJoins: result[0].joins })
+    //         })
+    // })
     await delay(150)
     res.render('main/index', input)
 })
--- router/projects/funshop.js
+++ router/projects/funshop.js
@@ -8,28 +8,28 @@ router.get('/:lang/projects/funshop/', async (req, res) => {
         return res.status(404).render('err')
     }
 
-    await mongoose.connect(mongodbIP).then(async () => {
-        const joins = await mongoose.connection.db.collection('joins')
+    // await mongoose.connect(mongodbIP).then(async () => {
+    //     const joins = await mongoose.connection.db.collection('joins')
 
-        await joins.findOneAndUpdate(
-            { link: `/${req.params.lang}/projects/funshop/` },
-            {
-                $inc: {
-                    joins: 1,
-                },
-            },
-            { new: true },
-        )
-        await joins.findOneAndUpdate(
-            { link: '/:lang/projects/funshop/' },
-            {
-                $inc: {
-                    joins: 1,
-                },
-            },
-            { new: true },
-        )
-    })
+    //     await joins.findOneAndUpdate(
+    //         { link: `/${req.params.lang}/projects/funshop/` },
+    //         {
+    //             $inc: {
+    //                 joins: 1,
+    //             },
+    //         },
+    //         { new: true },
+    //     )
+    //     await joins.findOneAndUpdate(
+    //         { link: '/:lang/projects/funshop/' },
+    //         {
+    //             $inc: {
+    //                 joins: 1,
+    //             },
+    //         },
+    //         { new: true },
+    //     )
+    // })
 
     let input = {
         lang: req.params.lang,
@@ -37,19 +37,19 @@ router.get('/:lang/projects/funshop/', async (req, res) => {
         username: req.session.username,
     }
 
-    await mongoose.connect(mongodbIP).then(async () => {
-        const joins = await mongoose.connection.db.collection('joins')
-        await joins
-            .find({ link: '/:lang/projects/funshop/' })
-            .toArray((err, result) => {
-                Object.assign(input, { all: result[0].joins })
-            })
-        await joins
-            .find({ link: `/${req.params.lang}/projects/funshop/` })
-            .toArray((err, result) => {
-                Object.assign(input, { langJoins: result[0].joins })
-            })
-    })
+    // await mongoose.connect(mongodbIP).then(async () => {
+    //     const joins = await mongoose.connection.db.collection('joins')
+    //     await joins
+    //         .find({ link: '/:lang/projects/funshop/' })
+    //         .toArray((err, result) => {
+    //             Object.assign(input, { all: result[0].joins })
+    //         })
+    //     await joins
+    //         .find({ link: `/${req.params.lang}/projects/funshop/` })
+    //         .toArray((err, result) => {
+    //             Object.assign(input, { langJoins: result[0].joins })
+    //         })
+    // })
     res.render('funshop/funshop', input)
 })
 module.exports = router
